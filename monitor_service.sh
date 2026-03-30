93c969385e5eedf9580f1ac1993ec1faf56d9deb
HSfina <hamssassia9@gmail.com>
2026-03-30 20:48:01 +0100

    add systemd service for continuous monitoring

---
+++ monitor_service.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /home/soufiane/linux-health-monitor/config.cfg
+DIR=~/linux-health-monitor
+
+echo "Health Monitor Service started at $(date)"
+
+while true; do
+    bash $DIR/cpu_check.sh
+    bash $DIR/ram_check.sh
+    bash $DIR/disk_check.sh
+    bash $DIR/network_check.sh
+    sleep 300  # 5 minutes
+done
