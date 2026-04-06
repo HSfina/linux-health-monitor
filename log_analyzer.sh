0083dbd150a2f05e5789a44ee2595ce1c0e68398
HSfina <hamssassia9@gmail.com>
2026-04-06 17:44:21 +0100

    enhance log analyzer with brute force detection and process monitoring

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -31,6 +31,9 @@ echo "-------------------------------------------------"
 ERR=$(journalctl -p err --since "today" --no-pager | awk '{print $5}' | sort | uniq -c | sort -rn | head -3)
 echo "	Top errors today	: $ERR"
 
+SSH=$(grep "Failed password" /var/log/auth.log | awk '{print $11}' | sort | uniq -c | sort -rn | awk '$1 > 5')
+echo "	Number of failed SSH	: $SSH"
+
 echo ""
 echo "SERVICES"
 echo "-------------------------------------------------"
@@ -38,8 +41,21 @@ echo "-------------------------------------------------"
 UPTIME=$(systemctl status health_monitor.service | grep "Active:" | awk '{print $2, $3, $4, $5, $6, $7, $8, $9}')
 echo "	Health Monitor         : $UPTIME"
 
+USERS=$(who | awk '{print $1}' | sort | uniq -c)
+echo "	Connected users at the moment : $USERS"
+
 LAST_CHECK=$(tail -1 ~/linux-health-monitor/service.log | awk '{print $1, $2}')
 echo "	Last check             : $LAST_CHECK"
+
+CNX=$(last -n 5)
+echo "	Last 5 successful connections	: $CNX"
+
+SUS_PROCCESSES=$(ps aux | awk '$3 > 50 {print $1, $2, $3, $11}')
+if [ -z $SUS_PROCCESSES ]; then
+	echo "	Suspicious processes	: None detected"
+else
+	echo "	Suspicious processes  : ⚠️  $SUS_PROCESSES"
+fi
 
 echo ""
 echo "================================================="
